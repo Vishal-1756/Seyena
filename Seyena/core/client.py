8550acb4c7c20fa189ab5f02189e8c0bcd960a69
Mukund <mukundsrajput@gmail.com>
2024-03-26 13:40:07 +0530

    ✓

--- Seyena/core/client.py
+++ Seyena/core/client.py
@@ -6,7 +6,7 @@ from config import Config
 class Seyena(Client):
     def __init__(self, name: str, api_id: int | str = None, api_hash: str = None, bot_token: str = None, session_string: str = None, plugins: dict = None, user:bool=True):
         self.user = user
-        if self.user == True:
+        if self.user is True:
             super().__init__(
                 name=name,
                 api_id=api_id,
@@ -31,7 +31,7 @@ class Seyena(Client):
         pro = await super().start()
         self.start_time = time.time()
         user = await pro.get_me()
-        if self.user == True:
+        if self.user is True:
             Config.set_user_id(user.id)
         else:
             Config.set_bot_username(user.username)
